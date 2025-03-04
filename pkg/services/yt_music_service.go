1b0eee9d38b00aff6791f021786f9522ebb98f0c
Ratthapon Sananaur <poom8165@gmail.com>
2025-03-04 21:34:32 +0700

    add health check, logging

--- pkg/services/yt_music_service.go
+++ pkg/services/yt_music_service.go
@@ -1,6 +1,8 @@
 package services
 
 import (
+	"fmt"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/poomrtp/go-yt-music/pkg/utils"
 	"github.com/raitonoberu/ytmusic"
@@ -19,10 +21,11 @@ func NewYTMusicService() YTMusicService {
 func (s *ytMusicServiceImpl) Search(c *fiber.Ctx) error {
 	searchQ := c.Query("search")
 	searchClient := ytmusic.Search(searchQ)
-
+	fmt.Printf("[INFO] Search query: %v\n", searchQ)
 	result, err := searchClient.Next()
 	if err != nil {
-		return err
+		fmt.Printf("[ERROR] Search: %v\n", err)
+		return utils.BadRequestResponse(c, "Cannot search: ", err)
 	}
 
 	return utils.SendResponse(c, fiber.StatusOK, "Search result", result)
