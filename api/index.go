7e57efc1d21aaf3f7b3680d86a11af2bf6dd7a8f
Ratthapon Sananaur <poom8165@gmail.com>
2025-03-04 19:20:44 +0700

    [conf]: add serverless route

---
+++ api/index.go
@@ -0,0 +1,31 @@
+package handler
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/joho/godotenv"
+	"github.com/poomrtp/go-yt-music/internal/handlers"
+	"github.com/poomrtp/go-yt-music/internal/services"
+)
+
+// Handler - handles serverless function requests
+func Handler(w http.ResponseWriter, r *http.Request) {
+	godotenv.Load()
+	app := fiber.New()
+
+	// Setup CORS
+	app.Use(cors.New())
+
+	// Initialize services
+	ytMusicService := services.NewYTMusicService()
+
+	// Setup routes
+	ytMusicHandler := handlers.NewYTMusicHandler(ytMusicService)
+	ytMusicHandler.SetupRoutes(app)
+
+	app.Listen(fmt.Sprintf(":%s", os.Getenv("PORT")))
+}
